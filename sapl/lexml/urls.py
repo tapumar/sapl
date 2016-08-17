10669f71635f635bda4b1e843e7ad70ae9d587a2
Edward Ribeiro <eribeiro@interlegis.leg.br>
2016-08-17 15:14:59 -0300

    Montagem da infra do LexML (em andamento)

--- sapl/lexml/urls.py
+++ sapl/lexml/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import include, url
 
-from sapl.lexml.views import LexmlProvedorCrud, LexmlPublicadorCrud
+# from sapl.lexml.views import LexmlProvedorCrud, LexmlPublicadorCrud
+
+from .views import add_oai_server, edit_oai_server
 
 from .apps import AppConfig
 
 app_name = AppConfig.name
 
 urlpatterns = [
-    url(r'^sistema/lexml/provedor/',
-        include(LexmlProvedorCrud.get_urls())),
-    url(r'^sistema/lexml/publicador/',
-        include(LexmlPublicadorCrud.get_urls())),
+    # url(r'^sistema/lexml/provedor/',
+    #     include(LexmlProvedorCrud.get_urls())),
+    # url(r'^sistema/lexml/publicador/',
+    #     include(LexmlPublicadorCrud.get_urls())),
+    url(r'^sistema/lexml/OAI/add', add_oai_server),
+    url(r'^sistema/lexml/OAI/edit', edit_oai_server)
 ]
--- sapl/lexml/views.py
+++ sapl/lexml/views.py
@@ -1,6 +1,17 @@
-from sapl.crud.base import CrudAux
+# from sapl.crud.base import CrudAux
 
-from .models import LexmlProvedor, LexmlPublicador
+# from .models import LexmlProvedor, LexmlPublicador
+from django.shortcuts import render
 
-LexmlProvedorCrud = CrudAux.build(LexmlProvedor, 'lexml_provedor')
-LexmlPublicadorCrud = CrudAux.build(LexmlPublicador, 'lexml_publicador')
+
+# LexmlProvedorCrud = Crud.build(LexmlProvedor, 'lexml_provedor')
+# LexmlPublicadorCrud = Crud.build(LexmlPublicador, 'lexml_publicador')
+
+def add_oai_server(request):
+    template = 'lexml/add_SAPLOAIServer.html'
+    return render(request, template, {})
+
+
+def edit_oai_server(request):
+    template = 'lexml/edit_SAPLOAIServer.html'
+    return render(request, template, {})
